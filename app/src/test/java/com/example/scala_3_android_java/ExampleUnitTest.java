2f8005d1b085335fcb666d51e229f0ffe1e996e6
Mario Càllisto <mario.callisto@gmail.com>
2025-04-08 09:02:15 +0200

    Trying Scala failing test

--- app/src/test/java/com/example/scala_3_android_java/ExampleUnitTest.java
+++ app/src/test/java/com/example/scala_3_android_java/ExampleUnitTest.java
@@ -16,7 +16,7 @@ import scala.jdk.javaapi.OptionConverters;
 public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() {
-        assertEquals(5, 2 + 2);
+        assertEquals(4, 2 + 2);
     }
 
     @Test
---
+++ app/src/test/scala/com/example/scala_3_android_java/PippoUnitTest.scala
@@ -0,0 +1,17 @@
+package com.example.scala_3_android_java
+
+import org.junit.Test
+import org.junit.Assert._
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class PippoUnitTest {
+  @Test
+  def addition_isCorrect(): Unit = {
+    assertEquals(5, 2 + 2)
+  }
+}
+
